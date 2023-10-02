d4806f05fa787ba7ed6deb12abcb9f6d0703180e
Paul Buzakov <buzakovpa@gmail.com>
2023-10-02 11:11:56 +0500

    feat: Add oh-my-posh custom style for Fish and PowerShell
    On branch feature
    Changes to be committed:
        modified:   fish/config.fish
        new file:   oh-my-posh/catppuccin_mocha.omp.json
        modified:   powershell/Microsoft.PowerShell_profile.ps1

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,3 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    oh-my-posh init fish --config '~/.config/oh-my-posh/catppuccin_mocha.omp.json' | source
 end
--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
+$(/opt/homebrew/bin/brew shellenv) | Invoke-Expression
+
+oh-my-posh init pwsh --config '~/.config/oh-my-posh/catppuccin_mocha.omp.json' | Invoke-Expression
+
 New-Alias -Name powershell -Value pwsh
 New-Alias -Name vim -Value nvim
 New-Alias -Name g -Value git
